16eb3fcdeebb0e22a31460b7b525fb6d0093c085
EMelo13 <55262301+EMelo13@users.noreply.github.com>
2019-11-18 15:45:50 -0500

    Step 8
    Fixin up the menu and game scenes

---
+++ New Multiplayer Demo/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class GameController : NetworkBehaviour
+{
+    public GameObject enemyPrefab;
+
+    private float spawnEnemyTime = 0;
+
+    void Update()
+    {
+        // We only want the server to handle this...
+        if (isServer)
+        {
+            if (Time.fixedTime > spawnEnemyTime)
+            {
+                SpawnEnemy();
+            }
+        }
+    }
+
+    [Server]
+    public void SpawnEnemy()
+    {
+        Vector3 position = new Vector3(Random.Range(-6.75f, 6.75f), Random.Range(1.0f, 8.0f), 4.5f);
+        GameObject enemy = (GameObject)Instantiate(enemyPrefab, position, Quaternion.identity);
+        NetworkServer.Spawn(enemy);
+        spawnEnemyTime = Time.fixedTime + Random.Range(3, 8);
+    }
+}

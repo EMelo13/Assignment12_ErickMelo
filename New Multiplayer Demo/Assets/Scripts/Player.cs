cb327cea4b1568a324cdcc5735bf472c7605800d
EMelo13 <55262301+EMelo13@users.noreply.github.com>
2019-11-18 15:21:38 -0500

    Step 4
    Movin' and stuff

--- New Multiplayer Demo/Assets/Scripts/Player.cs
+++ New Multiplayer Demo/Assets/Scripts/Player.cs
@@ -6,8 +6,28 @@ public class Player : NetworkBehaviour
     [SyncVar]
     public Color color;
 
+    float moveSpeed = 1.875f;
+
+
     public override void OnStartClient()
     {
         gameObject.GetComponent<Renderer>().material.color = color;
+    }
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        float y = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        MoveIt(x, y);
+    }
+
+    void MoveIt(float x, float y)
+    {
+        transform.Translate(x, y, 0);
     }
 }

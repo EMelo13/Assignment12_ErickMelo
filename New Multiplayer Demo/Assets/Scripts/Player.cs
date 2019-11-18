0e93c032f6fc8fc0c5c77a552cdbcfd3e9dd2d9a
EMelo13 <55262301+EMelo13@users.noreply.github.com>
2019-11-18 15:19:02 -0500

    Step 3
    Colors and stuff

--- New Multiplayer Demo/Assets/Scripts/Player.cs
+++ New Multiplayer Demo/Assets/Scripts/Player.cs
@@ -5,4 +5,9 @@ public class Player : NetworkBehaviour
 {
     [SyncVar]
     public Color color;
+
+    public override void OnStartClient()
+    {
+        gameObject.GetComponent<Renderer>().material.color = color;
+    }
 }

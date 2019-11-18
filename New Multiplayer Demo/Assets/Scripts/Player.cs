2f9c7052239b944749ed92c4c9f2f73935c354c6
EMelo13 <55262301+EMelo13@users.noreply.github.com>
2019-11-18 15:52:51 -0500

    Step 11
    Score!

--- New Multiplayer Demo/Assets/Scripts/Player.cs
+++ New Multiplayer Demo/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using Mirror;
 
 public class Player : NetworkBehaviour
@@ -11,19 +12,23 @@ public class Player : NetworkBehaviour
 
     float moveSpeed = 1.875f;
     public GameObject bulletPrefab;
+    private Text scoreText;
 
     public override void OnStartClient()
     {
         gameObject.GetComponent<Renderer>().material.color = color;
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
     }
 
     void Update()
     {
-        if (isLocalPlayer)
+        if (isLocalPlayer && hasAuthority)
         {
             GetInput();
+            scoreText.text = "Score: " + score;
         }
     }
+
 
     void GetInput()
     {

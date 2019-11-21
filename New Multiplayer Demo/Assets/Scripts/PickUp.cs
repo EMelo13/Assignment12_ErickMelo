91286c1b61355f02c40dd2abeb99bc8bad7311b4
EMelo13 <55262301+EMelo13@users.noreply.github.com>
2019-11-20 20:11:52 -0500

    Pick up
    (Messed around with it, does not appear in scene)

---
+++ New Multiplayer Demo/Assets/Scripts/PickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class PickUp : NetworkBehaviour
+{
+    float speed = 50;
+ 
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+            PlayerHealth playerHealth = new PlayerHealth(+20);
+           
+    }
+}
